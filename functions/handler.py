001773db8ec168ff30624b46f7688be16f5475f4
shlomik-cbrix <81581678+shlomik-cbrix@users.noreply.github.com>
2021-10-31 18:56:34 +0200

    add issues

--- functions/handler.py
+++ functions/handler.py
@@ -8,4 +8,14 @@ def get(event, context):
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
+    domain = input("Enter the Domain: ")
+    output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+    print(output)
+
+    secret="super secret"
+
+    domain = input("111 the Domain:222 ")  # 123
+    output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+    print(output)
+
     return response
